ff86dfc608f40fd8657858283b31f8752b819ee1
aliamri <aliamribardi1@gmail.com>
2023-08-18 22:23:45 +0800

    .

--- Database/database.go
+++ Database/database.go
@@ -1,17 +1,22 @@
 package Database
 
 import (
-	"log"
+	  "os"
+	  "log"
   	"gorm.io/driver/mysql"
   	"gorm.io/gorm"
+    "github.com/joho/godotenv"
 )
 
 var DB *gorm.DB
 
 func ConnectionDatabase() {
+  godotenv.Load()
+
+  DB_Name := os.Getenv("DB_Name")
 	
   // refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
-  dsn := "root:@tcp(127.0.0.1:3306)/go-crud-gin?charset=utf8mb4&parseTime=True&loc=Local"
+  dsn := "root:@tcp(127.0.0.1:3306)/" + DB_Name + "?charset=utf8mb4&parseTime=True&loc=Local"
   db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
   if err != nil {
 	log.Fatal("DB Connection Error")
